55ded15a269835d69851db350b0009a635099b47
Johana Romero <johana@Johana>
2024-11-10 18:35:27 -0300

    modelo bd

---
+++ prisma/migrations/20241109175709_article/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `article` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL,
+    `description` TEXT NOT NULL,
+    `price` DOUBLE NOT NULL,
+    `purchased` BOOLEAN NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
